38eb4ffb7ca4a76f2a37ce914828f84e1fb2ef44
Len Volk <lvolkov@outlook.com>
2022-10-06 10:45:39 -0400

    tst

--- 02/demos/Code/03-02 Win VM in SIG.ps1
+++ 02/demos/Code/03-02 Win VM in SIG.ps1
@@ -2,14 +2,40 @@
 # Set Subscription, RG Name etc.
 . '.\00 Variables.ps1'
 
+az login --only-show-errors -o table --query Dummy
+# Set Azure subscription
+az account set -s $subscription
+
 # Set Image Name
 $PrevImageName="aibImageJSON"
 $imageName="aibImageSIG"
 
+
+# Build addl. resource Names 
+$identityName="aib"+(Get-Random -Minimum 100000000 -Maximum 99999999999)
+$imageRoleDefName="Azure Image Builder Image Def"+(Get-Random -Minimum 100000000 -Maximum 99999999999)
+
+# Create resource group
+$RGScope=(az group create -n $aibRG -l $location --query id -o tsv)
+
+# Create Identity
+$Identity=(az identity create -g $aibRG -n $identityName) | ConvertFrom-Json
+$imgBuilderCliId=$Identity.clientId
+$imgBuilderId=$Identity.id
+
+# Get role definition, modify, create and assign
+$AzureRoleAIB = Get-Content 'AzureRoleAIB.json.dist' -raw | ConvertFrom-Json
+$AzureRoleAIB.Name=$imageRoleDefName
+$AzureRoleAIB.AssignableScopes[0]=$RGScope
+$AzureRoleAIB | ConvertTo-Json | Out-File "AzureRoleAIB.json"
+az role definition create --role-definition ./AzureRoleAIB.json
+
+az role assignment create --assignee $imgBuilderCliId --role $imageRoleDefName --scope $RGScope
+
 # Try to deploy old Image in new Region. Will fail
-az vm create --resource-group $aibRG --name VM_Old `
-        --admin-username $VM_User --admin-password $WinVM_Password `
-        --image $PrevImageName --location eastus --public-ip-sku Standard 
+# az vm create --resource-group $aibRG --name VM_Old `
+#         --admin-username $VM_User --admin-password $WinVM_Password `
+#         --image $PrevImageName --location eastus --public-ip-sku Standard 
 
 # Create Shared Image Gallery 
 $sigName="aibSig"
@@ -35,6 +61,7 @@ $dist.PSObject.Properties.Remove('imageId')
 $dist.PSObject.Properties.Remove('location')
 $dist | Add-Member -NotePropertyName galleryImageId -NotePropertyValue $SigDef
 $dist | Add-Member -NotePropertyName replicationRegions -NotePropertyValue $SIGLocations
+$TemplateJSON.identity.userAssignedIdentities = [pscustomobject]@{$imgBuilderId=[pscustomobject]@{}}
 $TemplateJSON.properties.distribute[0]=$dist
 $TemplateJSON | ConvertTo-Json -Depth 4 | Out-File "ImageTemplate-SIG.json" -Encoding ascii
 
@@ -54,20 +81,35 @@ az sig image-version list --gallery-image-definition $imageName `
 # Build the image again
 az image builder run -n $imageName -g $aibRG
 
-# Show image versions again
+# Show image versions again, it increased 
 az sig image-version list --gallery-image-definition $imageName `
                           --gallery-name $sigName `
                           --resource-group $aibRG -o table
 
 # Create VMs
+
 foreach($loc in $SIGLocations) {
 az vm create --resource-group $aibRG --name VM_SIG_$loc `
         --admin-username $VM_User --admin-password $WinVM_Password `
-        --image $SigDef/versions/latest --location $loc --public-ip-sku Standard
-}
+        --image $SigDef/versions/latest --location $loc --public-ip-sku Standard `
+        --tags 'demo=0302' `
+        --query publicIpAddress -o tsv } 
+
 
 # Check out VMs
 az vm list -g $aibRG -o table
+
+# Delete VMs
+$VMs=(az vm list -g $aibRG | ConvertFrom-Json)
+foreach($VM in $VMs) {
+    az vm delete -n $VM.name -g $aibRG --yes
+}
+ 
+# Delete other resources
+$Resources=(az resource list --tag 'demo=0302' | ConvertFrom-Json)
+foreach($res in $Resources) {
+    az resource delete -n $res.name -g $aibRG --resource-type $res.type
+}
 
 # Delete RG
 az group delete -g $aibRG --yes
