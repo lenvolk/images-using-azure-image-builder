b68fda98000467589b3310169e80a878363fe4f0
Len Volk <lvolkov@outlook.com>
2022-10-10 20:18:28 -0400

    test

--- LenVolk/avd_fslogix.ps1
+++ LenVolk/avd_fslogix.ps1
@@ -1,3 +1,6 @@
+# If done via custom extention
+# .\\wvd_fslogix.ps1 -ProfilePath "\\lvolkfiles.file.core.windows.net\garbage" - LocalWVDpath "C:\installers\FsLogix\x64\Release" -Verbose 
+
 ##########################################
 #    Log Function                        #
 ##########################################
@@ -13,6 +16,7 @@ function Write-Log {
 $LocalWVDpath            = "c:\temp\"
 $FSLogixURI              = 'https://aka.ms/fslogix_download'
 $FSInstaller             = 'FSLogixAppsSetup.zip'
+$ProfilePath             = "\\lvolkfiles.file.core.windows.net\garbage"
 
 #################################
 #    Download WVD Componants    #
@@ -48,7 +52,71 @@ catch {
 }
 
 
+#######################################
+#    FSLogix User Profile Settings    #
+#######################################
 
+try {
+
+    # Add FSLogix Profile Registry Value
+    New-ItemProperty -ErrorAction Stop `
+        -Path HKLM:\SOFTWARE\FSLogix\Profiles `
+        -Name "VHDLocations" `
+        -PropertyType Multistring `
+        -Value "$ProfilePath" `
+        -Force `
+        -Confirm:$false
+
+    Set-ItemProperty `
+        -Path HKLM:\Software\FSLogix\Profiles `
+        -Name "Enabled" `
+        -Type "Dword" `
+        -Value "1"
+    Set-ItemProperty `
+        -Path HKLM:\Software\FSLogix\Profiles `
+        -Name "SizeInMBs" `
+        -Type "Dword" `
+        -Value "10000"
+    Set-ItemProperty `
+        -Path HKLM:\Software\FSLogix\Profiles `
+        -Name "IsDynamic" `
+        -Type "Dword" `
+        -Value "1"
+    Set-ItemProperty `
+        -Path HKLM:\Software\FSLogix\Profiles `
+        -Name "VolumeType" `
+        -Type String `
+        -Value "vhdx"
+    Set-ItemProperty `
+        -Path HKLM:\Software\FSLogix\Profiles `
+        -Name "FlipFlopProfileDirectoryName" `
+        -Type "Dword" `
+        -Value "1" 
+    Set-ItemProperty `
+        -Path HKLM:\Software\FSLogix\Profiles `
+        -Name "SIDDirNamePattern" `
+        -Type String `
+        -Value "%username%%sid%"
+    Set-ItemProperty `
+        -Path HKLM:\Software\FSLogix\Profiles `
+        -Name "SIDDirNameMatch" `
+        -Type String `
+        -Value "%username%%sid%"
+    Set-ItemProperty `
+        -Path HKLM:\Software\FSLogix\Profiles `
+        -Name DeleteLocalProfileWhenVHDShouldApply `
+        -Type DWord `
+        -Value 1
+
+    Write-Output  "Done with FSLogix User Profile Settings"
+
+}
+
+catch {
+    $ErrorMessage = $_.Exception.message
+    write-log "Error adding profile settings registry KEY: $ErrorMessage"
+    Write-Output "***** Error adding profile settings registry KEY: $ErrorMessage"
+}
 
 
 
