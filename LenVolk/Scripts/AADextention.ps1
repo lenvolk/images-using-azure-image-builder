aedaede8d6c6aaf7eff0b30279dfb296ff369254
Len Volk <lvolkov@outlook.com>
2022-10-14 17:27:24 -0400

    tst

--- LenVolk/Scripts/AADextention.ps1
+++ LenVolk/Scripts/AADextention.ps1
@@ -17,11 +17,16 @@ Write-Output $resourceGroupName
 Write-Output $vmName
 Write-Output $location
 
+mkdir c:\ImageBuilder
+echo (Get-Date)  > c:\ImageBuilder\Timestamp.txt
+echo (Get-Date) | Out-File -FilePath c:\ImageBuilder\Timestamp.txt -Append
+$resourceGroupName | Out-File -FilePath c:\ImageBuilder\Timestamp.txt -Append
+$vmName | Out-File -FilePath c:\ImageBuilder\Timestamp.txt -Append
 
 # Azure AD Join domain extension
-$domainJoinName = "AADLoginForWindows"
-$domainJoinType = "AADLoginForWindows"
-$domainJoinPublisher = "Microsoft.Azure.ActiveDirectory"
-$domainJoinVersion   = "1.0"
+# $domainJoinName = "AADLoginForWindows"
+# $domainJoinType = "AADLoginForWindows"
+# $domainJoinPublisher = "Microsoft.Azure.ActiveDirectory"
+# $domainJoinVersion   = "1.0"
 
-Set-AzVMExtension -VMName $vmName -ResourceGroupName $resourceGroupName -Location $location -TypeHandlerVersion $domainJoinVersion -Publisher $domainJoinPublisher -ExtensionType $domainJoinType -Name $domainJoinName
+# Set-AzVMExtension -VMName $vmName -ResourceGroupName $resourceGroupName -Location $location -TypeHandlerVersion $domainJoinVersion -Publisher $domainJoinPublisher -ExtensionType $domainJoinType -Name $domainJoinName
