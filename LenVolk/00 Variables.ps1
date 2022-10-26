5eb5f890c9973d85a8e19b6ad6213ffc14cf3e91
Len Volk <lvolkov@outlook.com>
2022-10-26 09:28:33 -0400

    working state

--- LenVolk/00 Variables.ps1
+++ LenVolk/00 Variables.ps1
@@ -1,5 +1,6 @@
 # $subscription = "c6aa1fdc-66a8-446e-8b37-7794cd545e44"
 # Connect-AzAccount -Subscription $subscription
+# Set-AzContext -Subscription "AzIntConsumption"
 # Disconnect-AzAccount
 
 
--- LenVolk/DSC/0_import.ps1
+++ LenVolk/DSC/0_import.ps1
@@ -3,14 +3,40 @@
 #######################################
 #      IMPORT THE CONFIGURATION       #
 #######################################
+# Ref: https://itnext.io/azure-automation-configuring-desired-state-configuration-4091d94e85d9
+
 $grp="automation"
 Import-AzAutomationDscConfiguration -Published -ResourceGroupName $grp -SourcePath ./file.ps1 -Force -AutomationAccountName Automation
+# Compiling
+Start-AzAutomationDscCompilationJob `
+    -ConfigurationName 'file' `
+    -ResourceGroupName 'automation' `
+    -AutomationAccountName 'automation' `
+
+# Obtain metadata for DSC configuration and ensure RollupStatus is Good
+Get-AzAutomationDscNodeConfiguration `
+     -ResourceGroupName $AutomationRG `
+     -AutomationAccountName $AutomationAccount
+
 
 #######################################
 #            Enable DSC               #
 #######################################
+# Register the virtual machine - TST
+# Register-AzAutomationDscNode `
+#     -AzureVMName "Lab1SHv1-1" `
+#     -AzureVMLocation "eastus" `
+#     -AzureVMResourceGroup "lab1hprg" `
+#     -ResourceGroupName "automation"`
+#     -AutomationAccountName "automation" `
+#     -NodeConfigurationName "file.localhost" `
+#     -ConfigurationMode "ApplyAndAutocorrect" `
+#     -RefreshFrequencyMins "30" `
+#     -ActionAfterReboot "ContinueConfiguration" `
+#     -RebootNodeIfNeeded $True
 
-$VMRG = "WorkStation"
+
+$VMRG = "lab1hprg"
 $RunningVMs = (get-azvm -ResourceGroupName $VMRG -Status) | Where-Object { $_.PowerState -eq "VM running" -and $_.StorageProfile.OsDisk.OsType -eq "Windows" } 
 
 $AutomationAccount = "automation"
@@ -20,11 +46,18 @@ $RunningVMs | ForEach-Object -Parallel {
     Register-AzAutomationDscNode `
         -AzureVMName $_.Name `
         -AzureVMLocation $_.Location `
+        -AzureVMResourceGroup $_.ResourceGroupName `
+        -ResourceGroupName "automation"`
+        -AutomationAccountName "automation" `
         -NodeConfigurationName "file.localhost" `
         -ConfigurationMode "ApplyAndAutocorrect" `
-        -AutomationAccountName $using:AutomationAccount `
-        -ResourceGroupName $using:AutomationRG -Verbose
+        -RefreshFrequencyMins "30" `
+        -ActionAfterReboot "ContinueConfiguration" `
+        -RebootNodeIfNeeded $True
 }
+
+Get-AzAutomationDscNode -ResourceGroupName $AutomationRG -AutomationAccountName $AutomationAccount -NodeConfigurationName 'file.localhost'
+
 
 # $avdDscSettings = @{
 #     Name               = "Microsoft.PowerShell.DSC"
