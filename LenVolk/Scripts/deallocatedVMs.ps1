30dd2382c5811c69edbf78d787f8faeb40bdfbbd
Len Volk <lvolkov@outlook.com>
2022-11-04 15:53:01 -0400

    tst

--- LenVolk/Scripts/deallocatedVMs.ps1
+++ LenVolk/Scripts/deallocatedVMs.ps1
@@ -53,3 +53,32 @@ $StoppedVMs | ForEach-Object -Parallel {
 # $WinVM_Password = "P@ssw0rdP@ssw0rd"
 # cmdkey /generic:$VMIP /user:$VM_User /pass:$WinVM_Password
 # mstsc /v:$VMIP /w:1440 /h:900
+
+
+# $StoppedVMs = (get-azvm -ResourceGroupName "imageBuilderRG" -Status) | Where-Object { $_.PowerState -eq "VM stopped" -and $_.StorageProfile.OsDisk.OsType -eq "Windows" } 
+
+# $StoppedVMs | ForEach-Object -Parallel {
+#     try {
+#         Stop-AzVM -ErrorAction Stop -ResourceGroupName $_.ResourceGroupName -Name $_.Name -Force | Out-Null
+#         }
+#     catch {
+#         $ErrorMessage = $_.Exception.message
+#         Write-Error ("Error deallocating: " + $ErrorMessage)
+#         Break
+#     }
+# }
+
+# start-sleep -Seconds 180
+
+# $DeallocatedVMs = (get-azvm -ResourceGroupName "imageBuilderRG" -Status) | Where-Object { $_.PowerState -eq "VM deallocated" -and $_.StorageProfile.OsDisk.OsType -eq "Windows" } 
+
+# $DeallocatedVMs | ForEach-Object -Parallel {
+#     try {
+#         Start-AzVM -ErrorAction Stop -ResourceGroupName $_.ResourceGroupName -Name $_.Name | Out-Null
+#         }
+#     catch {
+#         $ErrorMessage = $_.Exception.message
+#         Write-Error ("Error Starting: " + $ErrorMessage)
+#         Break
+#     }
+# }
