1cce59a66a0b95f4cc3273cd42d1c6a18375b891
Len Volk <lvolkov@outlook.com>
2022-12-12 13:40:32 -0500

    working ad join

--- LenVolk/Scripts/AD_VMjoin_invoke.ps1
+++ LenVolk/Scripts/AD_VMjoin_invoke.ps1
@@ -5,44 +5,28 @@
 # From PS: net use \\dc1.lvolk.com\ipc$ /u:lvolk\lv <mypassword>
 
 
-$VMRG = "imageBuilderRG"
-$DomainName = "lvolk.com"
-$OUPath = "OU=PoolHostPool,OU=AVD,DC=lvolk,DC=com"
-$credential = Get-Credential lv@lvolk.com
+$extensionName = 'lvolkdomainjoin'
+$DomainName = 'lvolk.com'
+$OUPath = 'OU=PoolHostPool,OU=AVD,DC=lvolk,DC=com'
+$VMName = 'avd-win11-0'
+$credential = Get-Credential 'lv@lvolk.com'
+$VMRG = 'imageBuilderRG'
 
-# $RunningVMs = (get-azvm -ResourceGroupName $VMRG -Status) | Where-Object { $_.PowerState -eq "VM running" -and $_.StorageProfile.OsDisk.OsType -eq "Windows"} 
-
-# # $RunningVMs | ForEach-Object -Parallel {
-# #     Invoke-AzVMRunCommand `
-# #         -ResourceGroupName $_.ResourceGroupName `
-# #         -VMName $_.Name `
-# #         -CommandId 'RunPowerShellScript' `
-# #         -Parameter @{DomainName = $using:DomainName;
-# #                      OUPath = $using:OUPath;
-# #                      credential = $using:credential} `
-# #         -ScriptPath './AD_VMjoin_script.ps1'
-# # }
-
-# $RunningVMs | ForEach-Object -Parallel {
-#     Invoke-AzVMRunCommand `
-#     -ResourceGroupName $_.ResourceGroupName `
-#     -VMName $_.Name `
-#     -CommandId 'RunPowerShellScript' `
-#     -ScriptString { Add-Computer -DomainName $DomainName -OUPath $OUPath -Credential $credential -Force }
-# }
-
-###################################
-# Using Set-AzVMADDomainExtension 
-###################################
-
-# $extensionName = 'lvolkdomainjoin'
-# $DomainName = 'lvolk.com'
-# $OUPath = 'OU=PoolHostPool,OU=AVD,DC=lvolk,DC=com'
-# $VMName = 'avd-win11-01'
-# $credential = Get-Credential 'lvolk\lv'
-# $ResourceGroupName = 'imageBuilderRG'
- 
-# Set-AzVMADDomainExtension -Name $extensionName -DomainName $DomainName -OUPath $OUPath  -VMName $VMName -Credential $credential -ResourceGroupName $ResourceGroupName -JoinOption 0x00000001 -Restart -Verbose
+$RunningVMs = (get-azvm -ResourceGroupName $VMRG -Status) | Where-Object { $_.PowerState -eq "VM running" -and $_.StorageProfile.OsDisk.OsType -eq "Windows"} 
 
 
-$RunningVMs = (get-azvm -ResourceGroupName $VMRG -Status) | Where-Object { $_.PowerState -eq "VM running" -and $_.StorageProfile.OsDisk.OsType -eq "Windows"} | Select Name
+#Set-AzVMADDomainExtension -Name $extensionName -DomainName $DomainName -OUPath $OUPath  -VMName $VMName -Credential $credential -ResourceGroupName $ResourceGroupName -JoinOption 0x00000003 -Restart -Verbose
+
+$RunningVMs | ForEach-Object -Parallel {
+    Set-AzVMADDomainExtension `
+        -Name $using:extensionName `
+        -DomainName $using:DomainName `
+        -OUPath $using:OUPath `
+        -ResourceGroupName $_.ResourceGroupName `
+        -VMName $_.Name `
+        -Credential $using:credential `
+        -JoinOption 0x00000003 -Restart -Verbose
+
+}
+#ARM domain join
+# https://learn.microsoft.com/en-us/samples/azure/azure-quickstart-templates/vm-domain-join-existing/
