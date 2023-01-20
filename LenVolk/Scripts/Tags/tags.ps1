19a4a97cd6942bda5d863959cd22077ddc7a775c
Len Volk <lvolkov@outlook.com>
2023-01-20 15:40:44 -0500

    tst

--- LenVolk/Scripts/Tags/tags.ps1
+++ LenVolk/Scripts/Tags/tags.ps1
@@ -7,7 +7,7 @@ $tags = @{'PCI' = 'Yes'; 'Department'='Accounting'}
 
 foreach ($computer in $computers) { 
     Write-Host ".... Assigning $tags to VM Name $computer "
-    Update-AzTag -Tag $tags -ResourceId "/subscriptions/c6aa1fdc-66a8-446e-8b37-7794cd545e44/resourceGroups/fileservers/providers/Microsoft.Compute/virtualMachines/$computer" -Operation Merge -Verbose
+    Update-AzTag -Tag $tags -ResourceId "/subscriptions/<subID>/resourceGroups/<RGName>/providers/Microsoft.Compute/virtualMachines/$computer" -Operation Merge -Verbose
 }
 
 
