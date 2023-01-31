72271907ac5c680b295799a1d238a63a89bf2145
Len Volk <lvolkov@outlook.com>
2023-01-31 12:27:28 -0500

    tst

--- LenVolk/Scripts/AMA_DCR.ps1
+++ LenVolk/Scripts/AMA_DCR.ps1
@@ -7,13 +7,13 @@ $dcrname = 'Microsoft-VMInsights-Health-eastus'
 $rgname = 'lab1hprg'
 
 
-New-Item -Path . -Name dcrResources.txt -ItemType "file"
+#New-Item -Path . -Name dcrResources.txt -ItemType "file"
 $resources = (Get-AzDataCollectionRuleAssociation -ResourceGroupName $rgname -RuleName $dcrname).Id
 $resources | ForEach-Object {
     Write-Host $_.Split("/")[3] $_.Split("/")[8]
     $RGName = $_.Split("/")[3]
     $vmName = $_.Split("/")[8]
-    Add-Content -Path .\dcrResources.txt -Value "$RGName $vmName"
+    #Add-Content -Path .\dcrResources.txt -Value "$RGName $vmName"
 }
 
 # Check which LAW DCR is pointing to, from LAW run 
