fa92f3a7db1c5baba5ccbf1e4a4a2a44a72bd844
Len Volk <lvolkov@outlook.com>
2023-01-31 12:02:13 -0500

    tst

--- LenVolk/Scripts/AMA_DCR.ps1
+++ LenVolk/Scripts/AMA_DCR.ps1
@@ -1,12 +1,17 @@
+$SubscriptionId = "c6aa1fdc-66a8-446e-8b37-7794cd545e44"
+Connect-AzAccount -Subscription $SubscriptionId
+Set-AzContext -Subscription $SubscriptionId
+# Disconnect-AzAccount
+
 $dcrname = 'Microsoft-VMInsights-Health-eastus'
 $rgname = 'lab1hprg'
 
 
-# New-Item -Path . -Name dcrResources.txt -ItemType "file" $dcrname = 'arcdcr'
+New-Item -Path . -Name dcrResources.txt -ItemType "file"
 $resources = (Get-AzDataCollectionRuleAssociation -ResourceGroupName $rgname -RuleName $dcrname).Id
 $resources | ForEach-Object {
-    Write-Host $_.Split("/")[3] $_.Split("/")[8]
-    $RGName = $_.Split("/")[3]
-    $vmName = $_.Split("/")[8]
-    #Add-Content -Path .\dcrResources.txt -Value "$RGName $vmName"
+    Write-Host $_.Split("/")[3] $_.Split("/")[8]
+    $RGName = $_.Split("/")[3]
+    $vmName = $_.Split("/")[8]
+    Add-Content -Path .\dcrResources.txt -Value "$RGName $vmName"
 }
