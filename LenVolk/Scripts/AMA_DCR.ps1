e0abd52a6312e55e46216d030cc9c7963ee90830
Len Volk <lvolkov@outlook.com>
2023-01-31 21:45:44 -0500

    tst

--- LenVolk/Scripts/AMA_DCR.ps1
+++ LenVolk/Scripts/AMA_DCR.ps1
@@ -20,3 +20,14 @@ $resources | ForEach-Object {
 # Heartbeat | where Category == "Azure Monitor Agent" | distinct Computer
 
 # report on LAW agent https://argonsys.com/microsoft-cloud/library/how-to-find-your-azure-log-analytics-agent-deployments-in-preparation-for-the-azure-monitor-agent/
+$VMs = Get-AzVM 
+$WindowsVMs = $VMs | Where-Object { $PSItem.StorageProfile.ImageReference.Offer -eq "WindowsServer" }
+
+foreach ($VM in $WindowsVMs) {
+    $extension = Get-AzVMExtension -ResourceGroupName $Vm.ResourceGroupName -VMName $VM.Name
+
+    if ($extension.Name -contains "MicrosoftMonitoringAgent") {
+        Write-Host "Microsoft Monitoring Agent is Installed on" $VM.Name
+
+        }
+}
