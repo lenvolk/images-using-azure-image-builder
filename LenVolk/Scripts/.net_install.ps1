e91e9e591a32461deb58fbc578de29186343c249
Len Volk <lvolkov@outlook.com>
2023-02-02 15:23:58 -0500

    tst

---
+++ LenVolk/Scripts/.net_install.ps1
@@ -0,0 +1,3 @@
+#Go to the following registry key:
+HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AUIn the right-pane
+if the value named UseWUServer exists, set its data to 0
