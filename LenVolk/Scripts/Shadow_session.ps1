a0be2cf8d4eb9d5edafcce1a9d2521fb4f8cc729
Len Volk <lvolkov@outlook.com>
2023-02-06 17:35:27 -0500

    shadow session

---
+++ LenVolk/Scripts/Shadow_session.ps1
@@ -0,0 +1,28 @@
+
+#REF https://robinhobo.com/how-to-shadow-an-active-user-session-in-windows-virtual-desktop-via-remote-desktop-connection-mstc/
+
+$WinstationsKey = 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server'
+$name1 = 'fDenyTSConnections'
+
+if ((Get-ItemProperty $WinstationsKey).PSObject.Properties.Name -contains $name1) {
+     Write-Output "updating regkey $name1"
+     New-ItemProperty -Path $WinstationsKey -Name $name1 -ErrorAction:SilentlyContinue -PropertyType:dword -Value 0 -Force
+}
+else {
+    Write-Output "***** regkey name: $name1 not found"
+}
+
+# # Ref http://remotedesktoprdp.com/force-single-session-allow-multiple-sessions-per-user
+# $name2 = 'fSingleSessionPerUser'
+# if ((Get-ItemProperty $WinstationsKey).PSObject.Properties.Name -contains $name2) {
+#     Write-Output "updating regkey $name2"
+#     New-ItemProperty -Path $WinstationsKey -Name $name2 -ErrorAction:SilentlyContinue -PropertyType:dword -Value 0 -Force
+# }
+# else {
+#    Write-Output "***** regkey name: $name2 not found"
+# }
+
+Netsh advfirewall firewall set rule group=”remote desktop” new enable=yes
+
+
+mstsc.exe /shadow:2 /v:ChocoWin11m365 /control #/noConsentPrompt
