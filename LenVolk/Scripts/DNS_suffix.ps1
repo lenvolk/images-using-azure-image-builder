7362aadfb7a45cb0ccf32253f38f315ccefb942d
Len Volk <lvolkov@outlook.com>
2023-02-08 11:03:15 -0500

    tst

--- LenVolk/Scripts/DNS_suffix.ps1
+++ LenVolk/Scripts/DNS_suffix.ps1
@@ -1,4 +1,9 @@
 #REF https://eddiejackson.net/lab/2022/03/08/powershell-add-dns-suffix-to-ethernet-connections/
+#
+# Add-WindowsCapability -Name Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0 -Online
+# Get-Module -Name ActiveDirectory -ListAvailable
+# $DnsSufix = $(get-addomain).dnsroot
+#
 # ncpa.cpl
 # Properties
 # Internet protocol version 4
