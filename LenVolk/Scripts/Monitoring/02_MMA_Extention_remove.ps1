445cea5c3b44a5feada361fdc07025bfc09591ea
Len Volk <lvolkov@outlook.com>
2023-02-22 14:28:41 -0500

    tst

--- LenVolk/Scripts/Monitoring/02_MMA_Extention_remove.ps1
+++ LenVolk/Scripts/Monitoring/02_MMA_Extention_remove.ps1
@@ -14,3 +14,19 @@ foreach ($vmName in $computers) {
         Write-Output "$vmName VM not found"
     }
 }
+
+
+#########################
+#     Azure Arc MMA     #
+#########################
+# $subid = "ca5dfa45-eb4e-4612-9ebd-06f6fc3bc996"
+# Set-AzContext -Subscription $subid
+$PathToCsv = "C:\Temp\MMA_Arc.csv"
+$computers = (Import-Csv -Path $PathToCsv).vmname
+
+# For Azure VMs
+foreach ($vmName in $computers) { 
+
+        Remove-AzConnectedMachineExtension -MachineName $vmName -ResourceGroupName "AzureARCLAB" -Name OmsAgentforLinux -NoWait
+        Remove-AzConnectedMachineExtension -MachineName $vmName -ResourceGroupName "AzureARCLAB" -Name AzureMonitorLinuxAgent -NoWait
+}
