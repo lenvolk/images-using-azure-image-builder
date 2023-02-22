3afb6ada02b7fa300a84a691e154ec9fd0513660
Len Volk <lvolkov@outlook.com>
2023-02-21 20:35:03 -0500

    TY Preston

--- LenVolk/Scripts/Monitoring/02_MMA_Extention_remove.ps1
+++ LenVolk/Scripts/Monitoring/02_MMA_Extention_remove.ps1
@@ -1,19 +1,14 @@
 
 
 $PathToCsv = "C:\Temp\MMA_VMs.csv"
-$computers = Import-Csv -Path $PathToCsv | select VmName
-
-$vms = ($computers.psobject.Properties | Select Value)
-
-# Get-AzVM -Name $vms
-
+$computers = (Import-Csv -Path $PathToCsv).vmname
 
 # For Azure VMs
-foreach ($vmName in $vms) { 
+foreach ($vmName in $computers) { 
     $vmAzure = Get-AzVM -Name $vmName
     if ($vmAzure) {
-        Write-Output "Removing MMA agent"
-        Remove-AzVMExtension -ResourceGroupName $vmAzure.ResourceGroupName -Name MicrosoftMonitoringAgent -VMName $vmAzure.Name -Force
+        Write-Output "Removing MMA agent from $vmAzure"
+        #Remove-AzVMExtension -ResourceGroupName $vmAzure.ResourceGroupName -Name MicrosoftMonitoringAgent -VMName $vmAzure.Name -Force
     } 
     else {
         Write-Output "$vmName VM not found"
