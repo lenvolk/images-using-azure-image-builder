5a0650b5ba0284503a8639cf12e3b34d676fe1b6
Len Volk <lvolkov@outlook.com>
2023-02-22 14:59:59 -0500

    tst

--- LenVolk/Scripts/Monitoring/02_MMA_Extention_remove.ps1
+++ LenVolk/Scripts/Monitoring/02_MMA_Extention_remove.ps1
@@ -22,11 +22,12 @@ foreach ($vmName in $computers) {
 # $subid = "ca5dfa45-eb4e-4612-9ebd-06f6fc3bc996"
 # Set-AzContext -Subscription $subid
 $PathToCsv = "C:\Temp\MMA_Arc.csv"
-$computers = (Import-Csv -Path $PathToCsv).vmname
+$computers = Import-Csv -Path $PathToCsv
 
 # For Azure VMs
 foreach ($vmName in $computers) { 
 
-        Remove-AzConnectedMachineExtension -MachineName $vmName -ResourceGroupName "AzureARCLAB" -Name OmsAgentforLinux -NoWait
-        Remove-AzConnectedMachineExtension -MachineName $vmName -ResourceGroupName "AzureARCLAB" -Name AzureMonitorLinuxAgent -NoWait
+        Remove-AzConnectedMachineExtension -MachineName $vmName.VmName -ResourceGroupName $vmName.ResourceGroupName -Name OmsAgentforLinux -NoWait
+        Remove-AzConnectedMachineExtension -MachineName $vmName.VmName -ResourceGroupName $vmName.ResourceGroupName -Name AzureMonitorLinuxAgent -NoWait
+        Write-Output "Machine Name: "$vmName.VmName" has MMA removed"
 }
