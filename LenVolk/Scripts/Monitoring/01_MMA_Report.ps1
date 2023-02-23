231a2aa301700f0fbe320b9a59463c4fb772551b
Len Volk <lvolkov@outlook.com>
2023-02-23 11:27:02 -0500

    tst

--- LenVolk/Scripts/Monitoring/01_MMA_Report.ps1
+++ LenVolk/Scripts/Monitoring/01_MMA_Report.ps1
@@ -29,36 +29,68 @@ $report | ft -AutoSize VmName, ResourceGroupName
 #Change the path based on your convenience
 $report | Export-CSV  "c:\temp\$reportName" –NoTypeInformation
 
-#########################
-#     Azure Arc MMA     #
-#########################
-# Ref https://github.com/MicrosoftDocs/azure-docs/blob/main/articles/azure-arc/servers/manage-vm-extensions-powershell.md
-# $subid = "ca5dfa45-eb4e-4612-9ebd-06f6fc3bc996"
-# Set-AzContext -Subscription $subid
-# Install-Module -Name Az.ConnectedMachine -Verbose -Force
-# Install the Resource Graph module from PowerShell Gallery
-# Install-Module -Name Az.ResourceGraph -Verbose -Force
 
-# Create Report Array
+#########################################
+#     Azure Arc MMA                     #
+#########################################
 $report = @()
 $reportName = "MMA_Arc.csv"
 
-$ArcMachines = Search-AzGraph -Query "Resources | where type =~ 'microsoft.hybridcompute/machines' | extend agentversion = properties.agentVersion | project name, agentversion, location, resourceGroup, subscriptionId"
+$VMs = Get-AzConnectedMachine
+# $WindowsServers = $VMs | Where-Object { $PSItem.StorageProfile.ImageReference.Offer -eq "WindowsServer" }
+# $WindowsVMs = $VMs | Where-Object  {$_.StorageProfile.OsDisk.OsType -eq "Windows" }
 
-foreach ($ArcName in $ArcMachines) { 
-     
+foreach ($VM in $VMs) {
+
     $ReportDetails = "" | Select VmName, ResourceGroupName
-    $extension = Get-AzConnectedMachineExtension -ResourceGroupName $ArcName.resourceGroup -MachineName $ArcName.Name
 
-    if (($extension.Name -like "AzureMonitor*") -or ($extension.Name -like "OMSAgent*")) {
-        Write-Output "$($ArcName.Name) has MMA"
-        $ReportDetails.VMName = $ArcName.Name 
-        $ReportDetails.ResourceGroupName = $ArcName.resourceGroup
-        $report+=$ReportDetails 
-    } 
+    $extension = Get-AzConnectedMachineExtension -ResourceGroupName $Vm.ResourceGroupName -MachineName $VM.Name
+
+    $extension | ForEach-Object {
+        if (($_.Name -like "AzureMonitor*") -or ($_.Name -like "OMSAgent*")) {
+            Write-Host "Microsoft Monitoring Agent is Installed on $($VM.Name) in the RG:" $($VM.ResourceGroupName)
+            # $ReportDetails.VMName = $vm.Name 
+            # $ReportDetails.ResourceGroupName = $vm.ResourceGroupName 
+            # $report+=$ReportDetails 
+        }
+    }
 }
 
 $report | ft -AutoSize VmName, ResourceGroupName
  
 #Change the path based on your convenience
 $report | Export-CSV  "c:\temp\$reportName" –NoTypeInformation
+
+# #########################################
+# #     Azure Arc MMA  with Azure Graph   #
+# #########################################
+# # Ref https://github.com/MicrosoftDocs/azure-docs/blob/main/articles/azure-arc/servers/manage-vm-extensions-powershell.md
+# # $subid = "ca5dfa45-eb4e-4612-9ebd-06f6fc3bc996"
+# # Set-AzContext -Subscription $subid
+# # Install-Module -Name Az.ConnectedMachine -Verbose -Force
+# # Install the Resource Graph module from PowerShell Gallery
+# # Install-Module -Name Az.ResourceGraph -Verbose -Force
+
+# # Create Report Array
+# $report = @()
+# $reportName = "MMA_Arc.csv"
+
+# $ArcMachines = Search-AzGraph -Query "Resources | where type =~ 'microsoft.hybridcompute/machines' | extend agentversion = properties.agentVersion | project name, agentversion, location, resourceGroup, subscriptionId"
+
+# foreach ($ArcName in $ArcMachines) { 
+     
+#     $ReportDetails = "" | Select VmName, ResourceGroupName
+#     $extension = Get-AzConnectedMachineExtension -ResourceGroupName $ArcName.resourceGroup -MachineName $ArcName.Name
+
+#     if (($extension.Name -like "AzureMonitor*") -or ($extension.Name -like "OMSAgent*")) {
+#         Write-Output "$($ArcName.Name) has MMA"
+#         $ReportDetails.VMName = $ArcName.Name 
+#         $ReportDetails.ResourceGroupName = $ArcName.resourceGroup
+#         $report+=$ReportDetails 
+#     } 
+# }
+
+# $report | ft -AutoSize VmName, ResourceGroupName
+ 
+# #Change the path based on your convenience
+# $report | Export-CSV  "c:\temp\$reportName" –NoTypeInformation
