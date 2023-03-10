de661b430409f2fc321de9e96a0863cc6f90449e
Len Volk <lvolkov@outlook.com>
2023-03-09 22:14:49 -0500

    tst

--- LenVolk/Security/AVD-Policies/deloyment.ps1
+++ LenVolk/Security/AVD-Policies/deloyment.ps1
@@ -1,3 +1,3 @@
 
 
-$definition = New-AzPolicyDefinition -ManagementGroupName "volk-SandBox" -Name "policy-deploy-diagnostics-avd-application-group" -Policy ..\AVD-Policies\monitoring\policy-definitions\policy-definition-es-deploy-diagnostics-avd-application-group.json
+$avdappgrp = New-AzPolicyDefinition -ManagementGroupName "volk-SandBox" -Name "policy-deploy-diagnostics-avd-application-group" -Policy ..\AVD-Policies\monitoring\policy-definitions\policy-definition-es-deploy-diagnostics-avd-application-group.json
