1c907c1dd55d3fbfc3a94879d017075c69d8d70f
Len Volk <lvolkov@outlook.com>
2023-03-12 09:37:41 -0500

    st

--- LenVolk/Security/AVD-Policies/deloyment.ps1
+++ LenVolk/Security/AVD-Policies/deloyment.ps1
@@ -1,3 +1,5 @@
 
 
 $avdappgrp = New-AzPolicyDefinition -ManagementGroupName "volk-SandBox" -Name "policy-deploy-diagnostics-avd-application-group" -Policy ..\AVD-Policies\monitoring\policy-definitions\policy-definition-es-deploy-diagnostics-avd-application-group.json
+
+$avdappgrp = New-AzPolicyDefinition -ManagementGroupName "volk-SandBox" -Name "diagnostic settings for File Services" -Policy ..\AVD-Policies\monitoring\policy-definitions\policy-definition-es-deploy-diagnostics-azure-files.json
