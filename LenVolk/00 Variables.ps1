844fba0fe305d40c76f7bea87d2f87f867550fff
Len Volk <lvolkov@outlook.com>
2023-03-12 11:06:00 -0500

    tst

--- LenVolk/00 Variables.ps1
+++ LenVolk/00 Variables.ps1
@@ -1,6 +1,33 @@
 
+# =================================================================================================
+# Set Environment for Deployment
+# =================================================================================================
+Write-Host "Getting Azure Cloud list..." -ForegroundColor Yellow
+$CloudList = (Get-AzEnvironment).Name
+Write-Host "Which Azure Cloud would you like to deploy to?"
+Foreach($cloud in $CloudList){Write-Host ($CloudList.IndexOf($cloud)+1) "-" $cloud}
+$select = Read-Host "Enter selection"
+$Environment = $CloudList[$select-1]
+Write-Host "Connecting to Azure... (Look for minimized or hidden window)" -ForegroundColor Yellow
+Connect-AzAccount -Environment $Environment | Out-Null
+Clear-Host
 
+# =================================================================================================
+# Set Tenant for Deployment
+# =================================================================================================
+[array]$Tenants = Get-AzTenant
+If ($Tenants.count -gt 1){
+    Write-Host "Which Azure Tenant would you like to deploy to?"
+    Foreach($Tenant in $Tenants){
+        Write-Host ($Tenants.Indexof($Tenant)+1) "-" $Tenant.Name
+    }
+    $TenantSelection = Read-Host "Enter selection"
+    $TenantId = ($Tenants[$TenantSelection-1]).Id
+    Clear-Host
+}
+else{$TenantId = $Tenants[0].Id}
 
+# =================================================================================================
 # PS 
 # $subscription = "c6aa1fdc-66a8-446e-8b37-7794cd545e44"
 # Connect-AzAccount -Subscription $subscription 
