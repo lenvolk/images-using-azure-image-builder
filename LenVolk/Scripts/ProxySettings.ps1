9431e2b64357c92535c72af93778946c0ac6a53a
Len Volk <lvolkov@outlook.com>
2023-05-03 15:10:21 -0400

    tst

--- LenVolk/Scripts/ProxySettings.ps1
+++ LenVolk/Scripts/ProxySettings.ps1
@@ -4,6 +4,9 @@ Param (
 )
 
 # $ProxyServer = "10.199.0.19:3128"
+# "\\lvolk.com\sysvol\lvolk.com\scripts\setproxy.bat"
+
+# reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v "ProxyEnable" /t REG_DWORD /d 1 /f
 
 New-ItemProperty -ErrorAction Stop `
 -path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" `
@@ -13,6 +16,8 @@ New-ItemProperty -ErrorAction Stop `
 -Force `
 -Confirm:$false
 
+### Set Proxy Server
+# reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v "ProxyServer" /t REG_SZ /d "10.199.0.19:3128" /f
 
 New-ItemProperty -ErrorAction Stop `
 -path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' `
