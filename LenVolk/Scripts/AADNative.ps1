efb94abeb53669c6f0b73c0049756a23be4257c9
Len Volk <lvolkov@outlook.com>
2023-05-22 20:38:49 -0400

    aad native

---
+++ LenVolk/Scripts/AADNative.ps1
@@ -0,0 +1,37 @@
+write-host "Configuring FSLogix"
+
+
+###################
+#    Variables    #
+###################
+$fileServer="<StorageAccountName>.file.core.windows.net"
+$user="localhost\<StorageAccountName>"
+$profileShare="\\$($fileServer)\<ProfileShareName>"
+$secret="<StorageAccountAccessKey>"
+
+
+###########################################
+#    Execute Command In SYSTEM Context    #
+###########################################
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "LsaCfgFlags" -Value 0 -force
+cmd.exe /c "cmdkey.exe /add:$fileServer /user:$($user) /pass:$($secret)"
+
+
+################
+#    Profile   #
+################
+New-Item -Path "HKLM:\SOFTWARE" -Name "FSLogix" -ErrorAction Ignore
+New-Item -Path "HKLM:\SOFTWARE\FSLogix" -Name "Profiles" -ErrorAction Ignore
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "Enabled" -Value 1 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VHDLocations" -Value $profileShare -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "ConcurrentUserSessions" -Value 1 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "DeleteLocalProfileWhenVHDShouldApply" -Value 1 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "FlipFlopProfileDirectoryName" -Value 1 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "IsDynamic" -Value 1 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "KeepLocalDir" -Value 0 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "ProfileType" -Value 0 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "SizeInMBs" -Value 30000 -force
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VolumeType" -Value "VHDX" -force
+
+
+write-host "Configuration Complete"
