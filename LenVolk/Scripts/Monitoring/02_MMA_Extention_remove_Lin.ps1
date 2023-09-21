6945e8e42a7df252f6c8b570d0fc98b025624241
Len Volk <lvolkov@outlook.com>
2023-09-21 15:00:53 -0400

    tst

--- LenVolk/Scripts/Monitoring/02_MMA_Extention_remove_Lin.ps1
+++ LenVolk/Scripts/Monitoring/02_MMA_Extention_remove_Lin.ps1
@@ -9,8 +9,6 @@ foreach ($vmName in $computers) {
     if ($vmAzure) {
         Write-Output "Removing MMA agent from $vmName"
         Remove-AzVMExtension -ResourceGroupName $vmAzure.ResourceGroupName -Name OmsAgentForLinux -VMName $vmAzure.Name -Force
-        #for linux
-        #Remove-AzVMExtension -ResourceGroupName $vmAzure.ResourceGroupName -Name OmsAgentForLinux -VMName $vmAzure.Name -Force
     } 
     else {
         Write-Output "$vmName VM not found"
