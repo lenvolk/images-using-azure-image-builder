6638c1d2d91971f45ec6290131631e546e9c2c39
Len Volk <lvolkov@outlook.com>
2023-10-22 18:16:36 -0400

    upaded iis

--- LenVolk/Scripts/IIS.ps1
+++ LenVolk/Scripts/IIS.ps1
@@ -1,4 +1,10 @@
+# Win11
+# Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole, IIS-WebServer, IIS-CommonHttpFeatures, IIS-ManagementConsole, IIS-HttpErrors, IIS-HttpRedirect, IIS-WindowsAuthentication, IIS-StaticContent, IIS-DefaultDocument, IIS-HttpCompressionStatic, IIS-DirectoryBrowsing
+
+# Server
 Add-WindowsFeature Web-Server
+
+# IIS
 Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $($env:computername)
 New-Item -ItemType directory -Path "C:\inetpub\wwwroot\images"
 New-Item -ItemType directory -Path "C:\inetpub\wwwroot\video"
