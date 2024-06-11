ab65745a6b6524cc40fe12d148966f7565a84359
Len Volk <lvolkov@outlook.com>
2024-06-11 14:24:43 -0400

    netapp comments update

--- LenVolk/Scripts/create-netapp-fileshare.sh
+++ LenVolk/Scripts/create-netapp-fileshare.sh
@@ -24,12 +24,15 @@ az netappfiles pool create --resource-group "avd-netapp" \
   --pool-name "avdpool" --service-level "Standard" --size 2
 
 # 03 Create an Active Directory connection (computer object is not yet created in AD, only after volume creation it will show up)
+# https://learn.microsoft.com/en-us/azure/azure-netapp-files/create-active-directory-connections
 aduser="AD_USER" # username only, no domain component
 adpass='AD_PASSWORD'
 dcipaddress="DC_IP_ADDR" # IPv4 address of the domain controller
 domainname="DOMAIN_NAME" # e.g. contoso.com
 site="SITE_NAME" # e.g. Default-First-Site-Name
 oupath="OU_PATH" # No DC component, e.g. OU=NetApp
+
+# SMB Server Name Prefix: For example, if the naming standard that your organization uses for file services is NAS-01, NAS-02, and so on, then you would use NAS for the prefix
 
 az netappfiles account ad add -g "avd-netapp" \
   --name "avdnetapp" --username $aduser \
