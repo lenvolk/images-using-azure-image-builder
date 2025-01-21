0e972cbfb427286d77fafa959a62f27fc4548624
Len Volk <lvolkov@outlook.com>
2025-01-21 13:24:44 -0500

    arc

--- LenVolk/ARC/RunCommand.ps1
+++ LenVolk/ARC/RunCommand.ps1
@@ -219,3 +219,29 @@ foreach ($server in $filteredServers) {
 
     }
 }
+
+#### TY Jeff Pigott for sharing the script to change ARC Update Options
+# Ref https://learn.microsoft.com/en-us/azure/azure-arc/servers/manage-agent?tabs=windows#microsoft-update-configuration
+# Install the required module
+# Install-Module Az.ConnectedMachine -Force
+
+ 
+foreach ($server in $filteredServers) {
+    $script = '$ServiceManager = (New-Object -com "Microsoft.Update.ServiceManager"); $ServiceManager.Services; $ServiceID = "7971f918-a847-4430-9279-4a52d1efe18d"; $ServiceManager.AddService2($ServiceId,7,"")'
+    Start-Job -ScriptBlock {
+        New-AzConnectedMachineRunCommand -ResourceGroupName $using:server.ResourceGroupName -SourceScript $using:script -RunCommandName "runGetInfo50" -MachineName $using:server.Name -Location $using:server.Location
+    }
+}
+ 
+# Wait for all jobs to complete and get the results
+$jobs = Get-Job
+$results = $jobs | ForEach-Object { Receive-Job -Job $_; Remove-Job -Job $_ }
+
+# to validate: Create an instance of the Microsoft.Update.ServiceManager COM object
+$ServiceManager = New-Object -ComObject "Microsoft.Update.ServiceManager"
+ 
+# List all update services and their statuses
+$services = $ServiceManager.Services | Select-Object Name, IsDefaultAUService
+ 
+# Display the services
+$services
