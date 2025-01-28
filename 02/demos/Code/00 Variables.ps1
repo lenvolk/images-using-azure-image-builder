23e942ed5c9ef9b8052c2edfbcba1a90b7363c75
Len Volk <lvolkov@outlook.com>
2025-01-27 21:13:26 -0500

    tst

--- 02/demos/Code/00 Variables.ps1
+++ 02/demos/Code/00 Variables.ps1
@@ -1,13 +1,13 @@
 # Ref https://powers-hell.com/2020/09/20/preparing-custom-image-templates-with-azure-image-builder-powershell/
 # Install-Module -Name Az.ImageBuilder -RequiredVersion 0.1.2
 #
-# $subscription = "c6aa1fdc-66a8-446e-8b37-7794cd545e44"
+# $subscription = "4f70665a-02a0-48a0-a949-f3f645294566"
 # Connect-AzAccount -Subscription $subscription
 # Disconnect-AzAccount
 
 
 $aibRG = "imageBuilderRG"
-$subscription = "c6aa1fdc-66a8-446e-8b37-7794cd545e44"
+$subscription = "4f70665a-02a0-48a0-a949-f3f645294566"
 $VM_User = "aibadmin"
 $WinVM_Password = "P@ssw0rdP@ssw0rd"
 $location = "eastus2"
